80acce3f75c906a18e7cb898d335867c010a9960
cerentolunay <cerentlny@gmail.com>
2025-12-25 19:11:15 +0300

    Fix MainMenu return from Pause

--- Assets/Scripts/Core/SceneController.cs
+++ Assets/Scripts/Core/SceneController.cs
@@ -1,34 +1,44 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using DPBomberman.Models; // ✅ ThemeType burada
 
 public class SceneController : MonoBehaviour
 {
     public void LoadDesert()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Level_Desert");
+        if (GameManager.Instance != null)
+            GameManager.Instance.StartGame(ThemeType.Desert);
+        else
+            SceneManager.LoadScene("Level_Desert");
     }
 
     public void LoadForest()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Level_Forest");
+        if (GameManager.Instance != null)
+            GameManager.Instance.StartGame(ThemeType.Forest);
+        else
+            SceneManager.LoadScene("Level_Forest");
     }
 
     public void LoadCity()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Level_City");
+        if (GameManager.Instance != null)
+            GameManager.Instance.StartGame(ThemeType.City);
+        else
+            SceneManager.LoadScene("Level_City");
     }
 
     public void LoadMainMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        if (GameManager.Instance != null)
+            GameManager.Instance.GoToMainMenu();
+        else
+            SceneManager.LoadScene("MainMenu");
     }
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+    public void QuitGame() => Application.Quit();
 }
